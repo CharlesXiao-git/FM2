3af8d16b49b0840d23681926b9082362d6690d38
Amina Celik <amina.celik@slalom.com>
2020-07-18 15:22:50 +1000

    Create Alert component

---
+++ frontend/src/components/Alert/Alert.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="alert-container">
+    <b-alert
+      :variant="variant"
+      dismissible
+      :show="dismissCountDown"
+      @dismissed="dismissCountDown=0"
+      @dismiss-count-down="countDownChanged"
+      fade
+    >
+      {{text}}
+    </b-alert>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+type AlertVariant = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+
+@Component
+export default class Alert extends Vue {
+  @Prop() private variant: AlertVariant
+  @Prop() private text: string
+
+  dismissCountDown = 10
+
+  countDownChanged (dismissCountDown: number) {
+    this.dismissCountDown = dismissCountDown
+  }
+}
+</script>
+
+<style>
+  .alert-container {
+    height: 50px;
+    margin-bottom: 15px;
+  }
+</style>
