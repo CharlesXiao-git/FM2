90060b003cc1d90d865c04d6262eeff9bb370be5
Amina Celik <amina.celik@slalom.com>
2020-08-10 20:35:08 +1000

    Refactor AuthService with auth helpers

---
+++ frontend/src/helpers/auth/UserHelpers.ts
@@ -0,0 +1,62 @@
+import { User } from '@/model/User'
+import { decodeToken } from '@/helpers/auth/StorageHelpers'
+
+export type DecodedToken = {
+  userId: string;
+  sub: string;
+  email: string;
+  preferredUnit: string;
+  userRole: string;
+  exp: number;
+  iss: string;
+}
+
+export type DecodedTokenStringValue = 'userId' | 'sub' | 'email' | 'preferredUnit' | 'userRole' | 'iss'
+
+function getTokenValue (value: DecodedTokenStringValue) {
+  const parsedToken: DecodedToken = decodeToken()
+  return (parsedToken !== null && parsedToken[value] !== undefined) ? parsedToken[value] : undefined
+}
+
+export function userId () {
+  return getTokenValue('userId')
+}
+
+export function userName () {
+  return getTokenValue('sub')
+}
+
+export function userEmail () {
+  return getTokenValue('email')
+}
+
+export function userRole () {
+  return getTokenValue('userRole')
+}
+
+export function userPreferredUnit () {
+  return getTokenValue('preferredUnit')
+}
+
+export function currentUser () {
+  const parsedToken: DecodedToken = decodeToken()
+  const user = new User()
+
+  if (parsedToken !== null) {
+    user.id = parsedToken.userId
+    user.username = parsedToken.sub
+    user.email = parsedToken.email
+    user.role = parsedToken.userRole
+    user.preferredUnit = parsedToken.preferredUnit
+  }
+
+  return user
+}
+
+export function userSessionValid () {
+  const parsedToken: DecodedToken = decodeToken()
+  if (parsedToken && Date.now() < parsedToken.exp * 1000) {
+    return true
+  }
+  return false
+}
