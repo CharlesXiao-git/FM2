cdeb054dcd19db00fd6fcda6e318b3fbe8ad61b9
James Boyce <james.boyce@slalom.com>
2020-08-13 14:14:32 +1000

    configure CORS some more

--- backend/src/main/java/com/freightmate/harbour/configuration/WebSecurityConfiguration.java
+++ backend/src/main/java/com/freightmate/harbour/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.freightmate.harbour.filter.JWTAuthorizationFilter;
 import com.freightmate.harbour.model.UserRole;
 import com.freightmate.harbour.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -45,5 +50,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
                 .csrf().disable();
 
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
