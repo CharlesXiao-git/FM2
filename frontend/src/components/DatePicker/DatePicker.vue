32658767ef49ca55642ff96ee364712c93963455
Amina Celik <amina.celik@slalom.com>
2020-08-14 12:17:31 +1000

    Expose defaultValue and minValue props and set default date as Today

--- frontend/src/components/DatePicker/DatePicker.vue
+++ frontend/src/components/DatePicker/DatePicker.vue
@@ -26,9 +26,11 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 export default class DatePicker extends Vue {
   @Prop() private label: string
   @Prop() private placeholder: string
+  @Prop() private minValue: Date
+  @Prop() private defaultValue: Date
 
-  value: Date = null
-  min: Date = new Date()
+  value: Date = this.defaultValue || new Date()
+  min: Date = this.minValue || new Date()
   dateFormatOptions: object = { year: 'numeric', month: 'numeric', day: 'numeric' }
   datePickerPlaceholder = this.placeholder || 'Select a date'
   hideHeader = true
