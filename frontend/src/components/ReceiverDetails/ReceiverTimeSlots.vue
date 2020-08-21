63cac190ff796512ed399ba9d09da30682753b5a
Amina Celik <amina.celik@slalom.com>
2020-08-21 11:51:43 +1000

    Add ReceiverTimeSlots component

---
+++ frontend/src/components/ReceiverDetails/ReceiverTimeSlots.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="mt-2">
+        <b-form-checkbox v-model="isSelected">Receiver time slots</b-form-checkbox>
+      </div>
+    </div>
+
+    <div v-if="isSelected" class="row padded-row">
+      <div class="col-lg-12 col-xl-5 mt-2 p-0">
+        <b-form-group
+            label="Receive on date"
+            label-align-sm="left"
+            label-for="receive-date"
+            label-class="font-weight-bold pt-2 p-0"
+        >
+          <DatePicker
+              name='receive-date'
+              :min-value="minDate"
+              @selected-date="handleReceivedDate"
+          />
+        </b-form-group>
+      </div>
+      <div class="col-lg-12 col-xl-7 mt-2 pl-0">
+        <TimeSlotPicker :base-date="selectedDate" @selected-slot="handleTimeSlot"></TimeSlotPicker>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import { subDays } from 'date-fns'
+import DatePicker from '@/components/DatePicker/DatePicker.vue'
+import TimeSlotPicker from '@/components/TimeSlotPicker/TimeSlotPicker.vue'
+import { TimeSlot } from '@/components/TimeSlotPicker/TimeSlot'
+
+@Component({
+  components: { DatePicker, TimeSlotPicker }
+})
+export default class ReceiverTimeSlots extends Vue {
+  isSelected = false
+  minDate: Date = subDays(new Date(), 1)
+  selectedDate: Date = null
+  timeSlot: TimeSlot = null
+
+  handleReceivedDate (receiveDate: Date) {
+    this.selectedDate = receiveDate
+  }
+
+  handleTimeSlot (timeSlot: TimeSlot) {
+    this.timeSlot = timeSlot
+    this.$emit('selected-time-slot', this.timeSlot)
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./ReceiverTimeSlots.scss" />
