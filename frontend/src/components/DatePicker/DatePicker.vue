6803fd5a925116b8ac0fbaf8198b580be4661f28
Amina Celik <amina.celik@slalom.com>
2020-08-25 12:42:39 +1000

    Add "showResetBtn" to DatePicker to allow conditional rendering of the reset btn

--- frontend/src/components/DatePicker/DatePicker.vue
+++ frontend/src/components/DatePicker/DatePicker.vue
@@ -8,7 +8,7 @@
     :placeholder="placeholder"
     :start-weekday="1"
     :hide-header="true"
-    reset-button
+    :reset-button="showResetBtn"
     reset-button-variant="outline-success"
     selected-variant="success"
     locale="en-GB"
@@ -24,6 +24,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 export default class DatePicker extends Vue {
   @Prop({ required: true }) private name: string
   @Prop({ default: 'Select a date' }) private placeholder: string
+  @Prop({ default: false }) private showResetBtn: boolean
   @Prop() private defaultValue: Date
   @Prop() private minValue: Date
 
